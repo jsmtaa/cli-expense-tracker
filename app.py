95f0c8b3ec451432aaa42bf307553738f919a7df
jsmtaa <200186120+jsmtaa@users.noreply.github.com>
2026-06-14 19:58:13 +0800

    refactor: segregate functions into its own roles

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 class App:
     def __init__(self, accounts, ui):
         self.accounts = accounts
@@ -5,55 +6,59 @@ class App:
 
     def run(self):
         while True:
+            self.ui._clear_display()
+            self.ui.display_actions()
             action = self.ui.get_action()
-            if action == 0:
-                return
-            elif action == 1:
-                account = self.log_expense()
-            elif action == 2:
-                account = self.log_income()   
-            elif action == 3:
-                source, destination, amount = self.transfer()
-            elif action == 4:
-                self.ui.display_accounts(self.accounts)
-            else:
-                raise ValueError
-            if action == 4:
-                continue
-            elif action != 3:
-                print(f"Your updated balance for account \"{account["name"]}\" is {account["balance"]}")
-            else:
-                print(f"Successfully transferred {amount} from {source["name"]} to {destination["name"]}.")
-                print("Your updated balance:")
-                print(f"\"{source["name"]}\": \"{source["balance"]}\"")
-                print(f"\"{destination["name"]}\": \"{destination["balance"]}\"")
+            
+            match action:
+                case 0: # Exit App
+                    return
+                case 1:
+                    self.expense_service()
+                case 2: 
+                    self.income_service()
+                case 3:
+                    self.transfer_service()
+                case 4: # Display accounts
+                    self.ui.display_accounts(self.accounts)
+                case _: # Exception
+                    raise ValueError
 
-    def log_expense(self):
-        expense = self.ui.get_expense()
-        account = self.get_account()
-        if expense > account["balance"]:
+            self.ui.prompt_before_next_action()
+
+    # Services
+    def expense_service(self):
+        expense_amount = self.ui.get_expense()
+        source_account = self.get_account("expense")
+        if expense_amount > source_account["balance"]:
             raise ValueError("You don't have enough balance!")
-        account["balance"] -= expense
-        return account
+        source_account["balance"] -= expense_amount
 
-    def log_income(self):
-        income = self.ui.get_income()
-        account = self.get_account()
-        account["balance"] += income
-        return account
+        self.ui._clear_display()
+        self.ui.display_updated_balance(source_account, "expense")
     
-    def transfer(self):
-        source_account = self.get_account()
-        destination_account = self.get_account()
-        amount = self.ui.get_transfer_amount()
-        if amount > source_account["balance"]:
+    def income_service(self):
+        income_amount = self.ui.get_income()
+        destination_account = self.get_account("income")
+        destination_account["balance"] += income_amount
+
+        self.ui._clear_display()
+        self.ui.display_updated_balance(destination_account)
+    
+    def transfer_service(self):
+        # Process transfer
+        source_account, destination_account = self.get_account("transfer")
+        transfer_amount = self.ui.get_transfer_amount()
+        if transfer_amount > source_account["balance"]:
             print("Not enough funds!")
             raise ValueError
-        source_account["balance"] -= amount
-        destination_account["balance"] += amount
-        return source_account, destination_account, amount
+        source_account["balance"] -= transfer_amount
+        destination_account["balance"] += transfer_amount
+        # Display updated accounts balance
+        self.ui._clear_display()
+        self.ui.display_updated_balance(source_account, destination_account, "transfer")
 
-    def get_account(self):
-        account = self.ui.select_account(self.accounts)
+    def get_account(self, path):
+        account = self.ui._select_account(self.accounts, path)
         return account
 
--- ui.py
+++ ui.py
@@ -1,30 +1,67 @@
+import os 
+
 class UI:
+    # Helpers
+    def _clear_display(self):
+        os.system("clear")
+
     def _get_input(self):
         return input("> ")
 
-    def get_action(self):
-        print("Choose an option from the menu:")
-        print("1 Log expense")
-        print("2 Log income")
-        print("3 Transfer")
-        print("4 Display accounts")
-        print("0 Exit program")
-        return int(self._get_input())
+    # Display
+    def display_actions(self):
+        print("Select an action:")
+        print("1. Log expense")
+        print("2. Log income")
+        print("3. Transfer")
+        print("4. Display accounts")
+        print("0. Exit program")
 
-    def select_account(self, accounts):
-        print("Choose an account: ")
-        
+    def get_action(self):
+        return int(self._get_input())
+    def display_updated_balance(self, first_account, second_account=None, transaction="default"):
+        match transaction:
+            case "default":
+                print(f"Your updated balance for '{first_account["name"]}' is {first_account["balance"]:.2f}")
+            case "transfer":
+                print("Your updated accounts balance:")
+                print(f"{first_account["name"]}: {first_account["balance"]}")
+                print(f"{second_account["name"]}: {second_account["balance"]}")
+
+    def display_account_selector(self, accounts):
         for i, account in enumerate(accounts):
             print(i+1, accounts[account]["name"])
 
-        account_index = int(self._get_input()) - 1
 
-        for i, account in enumerate(accounts):
-            if i != account_index:
-                continue
-            return accounts[account]
+    def _select_account(self, accounts, transaction):
+        indices = []
 
-        raise ValueError
+        match transaction:
+            case "expense":
+                print("Select a source account")
+                self.display_account_selector(accounts)
+                indices.append(int(self._get_input()) - 1)
+            case "income":
+                print("Select a destination account")
+                self.display_account_selector(accounts)
+                indices.append(int(self._get_input()) - 1)
+            case "transfer":
+                print("Select a source account:")
+                self.display_account_selector(accounts)
+                indices.append(int(self._get_input()) - 1)
+                print("Select a destination account:")
+                self.display_account_selector(accounts)
+                indices.append(int(self._get_input()) - 1)
+
+        
+        result = []
+        for index in indices:
+            for i, account in enumerate(accounts):
+                if i != index:
+                    continue
+                result.append(accounts[account])
+        
+        return result[0] if len(result) == 1 else result
 
     def get_expense(self):
         return float(input("Enter expense: "))
@@ -36,11 +73,14 @@ class UI:
         return float(input("Enter amount to transfer: "))
 
     def display_accounts(self, accounts):
-        print("ACCOUNTS")
+        print("ACTIVE ACCOUNTS")
         for account_id in accounts:
             account = accounts[account_id]
-            print(f"\n{account["name"]}: {account["balance"]}")
-            print(f"{account["type"]}")
+            print(f"\n{account["name"]} | {account["type"].capitalize()}")
+            print(f"AMT: {account["balance"]}")
 
     def display_balance(self, balance):
         print(f"Your balance is: {balance}")
+
+    def prompt_before_next_action(self):
+        input("\nPress anything to continue...")
